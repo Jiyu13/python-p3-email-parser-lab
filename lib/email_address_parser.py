93893e2f281b417787d541011dec26d28d4c6b37
Ziru Zou <jiyucoding@yahoo.com>
2023-02-24 13:44:44 -0500

    complete lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    
+    # | => or
+    def parse(self):
+        pattern = re.compile(r"[, ]+")
+        addresses_list = pattern.split(self.addresses)
+        return sorted(set([address for address in addresses_list if "@" in address]))
